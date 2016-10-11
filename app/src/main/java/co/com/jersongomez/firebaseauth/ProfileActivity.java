b559fb46a2f96519fff35ab1197bdd01a55efb08
Jerson Gomez <jerson.gomez@decameron.com>
2016-10-11 10:18:02 -0500

    Actualizacion de datos con databasereference

--- app/src/main/java/co/com/jersongomez/firebaseauth/ProfileActivity.java
+++ app/src/main/java/co/com/jersongomez/firebaseauth/ProfileActivity.java
@@ -5,10 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -16,16 +20,22 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
     private TextView textViewUserEmail;
     private Button buttonSalir;
 
+    private DatabaseReference databaseReference;
+    private EditText editTextNameFull, editTextAddress;
+    private Button buttonSave;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
         firebaseAuth = FirebaseAuth.getInstance();
-        if (firebaseAuth.getCurrentUser() != null) {
-            finish();
-            startActivity(new Intent(getApplicationContext(), LoginActivity.class));
-        }
+
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        editTextAddress = (EditText) findViewById(R.id.editTextAddress);
+        editTextNameFull = (EditText) findViewById(R.id.editTextNameFull);
+        buttonSave = (Button) findViewById(R.id.btnAddProfile);
 
         FirebaseUser user = firebaseAuth.getCurrentUser();
 
@@ -34,6 +44,19 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
         buttonSalir = (Button) findViewById(R.id.btnSalir);
 
         buttonSalir.setOnClickListener(this);
+        buttonSave.setOnClickListener(this);
+    }
+
+    private void saveInformation() {
+        String name = editTextNameFull.getText().toString().trim();
+        String add = editTextAddress.getText().toString().trim();
+
+        UserInformation userInformation = new UserInformation(name, add);
+
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        databaseReference.child(user.getUid()).setValue(userInformation);
+        Toast.makeText(this, "Guardado con exito....", Toast.LENGTH_LONG).show();
     }
 
     @Override
@@ -43,6 +66,10 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
             finish();
             startActivity(new Intent(getApplicationContext(), LoginActivity.class));
 
+        }
+
+        if (v == buttonSave) {
+            saveInformation();
         }
     }
 }
---
+++ app/src/main/java/co/com/jersongomez/firebaseauth/UserInformation.java
@@ -0,0 +1,23 @@
+package co.com.jersongomez.firebaseauth;
+
+/**
+ * Created by jgomez on 11/10/16.
+ */
+
+public class UserInformation {
+
+    public String name;
+    public String address;
+
+    public UserInformation() {
+
+    }
+
+    public UserInformation(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+
+
+}
